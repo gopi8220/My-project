3bd28786118b2143ce9a966eb67db2d41a85c545
gopi8220 <148092701+gopi8220@users.noreply.github.com>
2023-11-16 08:46:14 -0800

    Add files via upload

---
+++ crud php/create.php
@@ -0,0 +1,95 @@
+<?php 
+
+include "config.php";
+
+  if (isset($_POST['submit'])) {
+
+    $first_name = $_POST['firstname'];
+
+    $last_name = $_POST['lastname'];
+
+    $email = $_POST['email'];
+
+    $password = $_POST['password'];
+
+    $gender = $_POST['gender'];
+
+    $sql = "INSERT INTO `boys`(`firstname`, `lastname`, `email`, `password`, `gender`) VALUES ('$first_name','$last_name','$email','$password','$gender')";
+
+    $result = $conn->query($sql);
+
+    if ($result == TRUE) {
+
+      echo "New record created successfully.";
+
+    }else{
+
+      echo "Error:". $sql . "<br>". $conn->error;
+
+    } 
+
+    $conn->close(); 
+
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <<link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Signup Form</h2>
+
+<form action="" method="POST">
+
+  <fieldset>
+    <align=center:"10px";>
+
+    <legend>Personal information:</legend>
+
+    First name:<br>
+
+    <input type="text" name="firstname">
+
+    <br>
+
+    Last name:<br>
+
+    <input type="text" name="lastname">
+
+    <br>
+
+    Email:<br>
+
+    <input type="email" name="email">
+
+    <br>
+
+    Password:<br>
+
+    <input type="password" name="password">
+
+    <br>
+
+    Gender:<br>
+
+    <input type="radio" name="gender" value="Male">Male
+
+    <input type="radio" name="gender" value="Female">Female
+
+    <br><br>
+
+    <input type="submit" name="submit" value="submit">
+
+  </fieldset>
+
+</form>
+
+</body>
+
+</html>
